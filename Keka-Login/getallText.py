00eb2c7480fb52099affc4c265d2ff5a4ca1c213
Shivammdh <67448585+Shivammdh@users.noreply.github.com>
2023-01-31 15:02:47 +0530

    Update getallText.py

--- Keka-Login/getallText.py
+++ Keka-Login/getallText.py
@@ -1,18 +1,27 @@
-# Importing necessary modules
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 import time
 
-# WebDriver Chrome
-driver = webdriver.Chrome()
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-# Target URL
-driver.get("https://www.geeksforgeeks.org/competitive-programming-a-complete-guide/")
-# To load entire webpage
-time.sleep(5)
+def test_kekalogin():
+    print('building session')
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(10)
+    print("-----------Keka Login page-----------")
+    driver.get("https://app.keka.com/Account/KekaLogin?returnUrl=%2F")
 
-# Printing the whole body text
-print(driver.find_element(By.XPATH, "/html/body").text)
+    vd=driver.find_element(By.CLASS_NAME,"form-control")
+    driver.execute_script("arguments[0].value='msys';",vd)
 
-# Closing the driver
-driver.close()
+    #send the email and password to these fields
+    driver.find_element(By.ID,"email").send_keys("xyz@gmail.com")
+
+    #driver.execute_script("document.getElementsByClassName('form-control').setAttribute('value', 'msys')")
+    driver.find_element(By.ID,"password").send_keys('Password123')
+    driver.find_element(By.CSS_SELECTOR,"button[type='submit']").click()
+    time.sleep(5)
+    name=driver.find_element(By.CSS_SELECTOR,"span.profile-name").text
+    print(f"Hi {name} You are successfully login on keka portal.")
+
+    ## DO STUFF
+    driver.quit()
